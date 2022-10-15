da0f1c65954237b4f3a54a6c9bdebd5bf578d7c2
Rishi Santhanam <rishisant@tamu.edu>
2022-10-15 17:19:48 -0500

    fixed linkers + base js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
 // SCRIPT.js
+// author @rishisant
+
+let topic = ""
+
+// what did you enter??
+function change() {
+    let input = document.getElementById("inpbox").value;
+    document.getElementById("checker").innerHTML = "you entered: " + input;
+}
+
+function chooseeasy() {
+    sessionStorage.setItem("easymode", "true")
+    sessionStorage.setItem("mediummode", "false")
+    sessionStorage.setItem("hardmode", "false")
+    sessionStorage.setItem("chainedmode", "false")
+    location.replace('./html/instructions.html')
+}
+
+function choosemedium() {
+    sessionStorage.setItem("mediummode", "true")
+    sessionStorage.setItem("easymode", "false")
+    sessionStorage.setItem("hardmode", "false")
+    sessionStorage.setItem("chainedmode", "false")
+    location.replace('./html/instructions.html')
+}
+
+function choosehard() {
+    sessionStorage.setItem("hardmode", "true")
+    sessionStorage.setItem("mediummode", "false")
+    sessionStorage.setItem("easymode", "false")
+    sessionStorage.setItem("chainedmode", "false")
+    location.replace('./html/instructions.html')
+}
+
+function choosechained() {
+    sessionStorage.setItem("chainedmode", "true")
+    sessionStorage.setItem("mediummode", "false")
+    sessionStorage.setItem("hardmode", "false")
+    sessionStorage.setItem("easymode", "false")
+    location.replace('./html/instructions.html')
+}
+
+function startgame() {
+    // set topic to something
+    // query from arr
+    // done
+    if (sessionStorage.getItem("easymode") == "true") {
+        location.replace('./easymode.html')
+    }
+    else if (sessionStorage.getItem("mediummode") == "true") {
+        location.replace('./mediummode.html')
+    }
+    else if (sessionStorage.getItem("hardmode") == "true") {
+        location.replace('./hardmode.html')
+    }
+    else {
+        location.replace('./chainedmode.html')
+    }
+}
